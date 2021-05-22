28a9675124c020ee36ab1007be6ba153bb5a1607
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-05-22 21:53:32 +0530

    Added os module
    Got the current path and generated the path for font and mask.
    
    Renamed Variables [font -> font_path & mask -> mask_path]

--- main.py
+++ main.py
@@ -3,8 +3,37 @@ import pyfiglet
 import wordcloud
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 #USER INTERACTION
 
 print(pyfiglet.figlet_format("Love Cloud",font="slant"))
 print(pyfiglet.figlet_format("By Subhodeep Sarkar",font="digital"))
+
+#TEXT GENERATION
+
+word_list = []
+print("Enter all the words you want to : )")
+print("To finish, type X and hit enter")
+
+counter = 0
+
+while True:
+    word = input(f"{counter+1}: ")
+    if word == "X":
+        break
+    word_list.append(word)
+    counter+=1
+
+if counter==1:
+    print(f"Added {counter} word")
+else:
+    print(f"Added {counter} words")
+
+text = " ".join(word_list)
+
+path = os.getcwd()
+font_path = os.path.join(path,"hb.ttf")
+mask_path = os.path.join(path,"romeo.jpg")
+
+

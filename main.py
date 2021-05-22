36f62526ecae41bf7ccdd585104480edd85c6edb
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-05-22 21:35:43 +0530

    Added welcome message

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pyfiglet
 import wordcloud
 import numpy as np
 import matplotlib.pyplot as plt
+
+#USER INTERACTION
+
+print(pyfiglet.figlet_format("Love Cloud",font="slant"))
+print(pyfiglet.figlet_format("By Subhodeep Sarkar",font="digital"))

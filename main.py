da9ee4c7904e65d4350e5a93857549bc95f1013a
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-05-22 22:04:08 +0530

    Generated & Displayed wordcloud

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import wordcloud
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import PIL
+from PIL import Image
 
 #USER INTERACTION
 
@@ -14,7 +14,7 @@ print(pyfiglet.figlet_format("By Subhodeep Sarkar",font="digital"))
 #TEXT GENERATION
 
 word_list = []
-print("Enter all the words you want to : )")
+print("Enter all the words you want to :)")
 print("To finish, type X and hit enter")
 
 counter = 0
@@ -31,11 +31,23 @@ if counter==1:
 else:
     print(f"Added {counter} words")
 
+print("Please wait while your Love Cloud is being created! :)")
+
 text = " ".join(word_list)
 
 path = os.getcwd()
 font_path = os.path.join(path,"hb.ttf")
 mask_path = os.path.join(path,"romeo.jpg")
 
-mask_image = np.array(pillow)
+mask_image = np.array(Image.open(mask_path))
 
+wc = wordcloud.WordCloud(
+                   mask=mask_image, background_color="white",
+                   max_words=1000, max_font_size=256, min_font_size=5,colormap='rainbow',
+                   random_state=42, width=600,
+                   height=400,contour_width=0.2, repeat=True,font_path=font_path).generate(text)
+
+plt.imshow(wc,interpolation='bilinear')
+plt.title('Love makes life perfect!')
+plt.axis('off')
+plt.show()
